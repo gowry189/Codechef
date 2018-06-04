69111c6e32f136db80cd46126b22f308e36f7418
gowry189 <gowry18@gmail.com>
2018-06-04 14:36:04 +0530

    Codechef

---
+++ findingsquareroots.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<math.h>
+int main()
+{
+    int t,i,a,s;
+    scanf("%d",&t);
+    for(i=0;i<t;i++)
+    {
+    scanf("%d",&a);
+    s=sqrt(a);
+    printf("%d\n",s);
+    }
+return 0;    
+}
